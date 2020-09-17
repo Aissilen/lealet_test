c3e231421532236c63288033a925600104c9718c
Aissilen <Ivanlobanov97@gmail.com>
2020-09-17 16:46:34 +0800

    all new function

--- script.js
+++ script.js
@@ -8,53 +8,87 @@ const map = new L.map('map',
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(map)
 
 let mapMarker = []
+let polyline
 
 let rkMarker = new L.Marker([52.28664250209102, 104.28634643554689],{
     type : 'rk',
-    draggable: true,
+    draggable: false,
 }).bindPopup('rk').addTo(map)
 
 let rsMarker = new L.Marker([52.282494522127635, 104.29347038269043],{
     type : 'rs',
-    draggable: true,
+    draggable: false,
 }).bindPopup('rs').addTo(map)
 
 mapMarker.push(rkMarker,rsMarker)
 
+polylinePrinter(mapMarker)
+
 map.addEventListener('click', event =>{
-    // console.log(event.latlng)
-    // console.log(rkMarker.getLatLng())
     if (mapMarker.length >= 2) {
         let bufMarker = mapMarker[mapMarker.length -1]
         let newMarker = new L.Marker(event.latlng,{
             type : '_newMarker_'+(mapMarker.length -1),
             draggable: true,
         }).bindPopup('rk').addTo(map)
-        
-        
+
+        addDragendListenerToMarkerAndRedrawPolyline(newMarker)
+
         mapMarker.pop()
         mapMarker.push(newMarker,bufMarker)
-        
         
         polylinePrinter(mapMarker)
     }
 })
-let polyline
 
 function polylinePrinter(markerArray){
     let latLngForPolyline = []
     markerArray.forEach(element => {
         latLngForPolyline.push(element._latlng)
     })
-    console.log(latLngForPolyline)
-    
-
-    
+       
     if (!polyline) {
-        console.log('123')
         polyline = L.polyline(latLngForPolyline, {color: 'red'}).addTo(map)
     }else{
         polyline.setLatLngs(latLngForPolyline)
         polyline.redraw()
     }
+}
+
+function addDragendListenerToMarkerAndRedrawPolyline(element){
+    element.addEventListener('dragend',event => {
+        polylinePrinter(mapMarker)
+    })
+}
+
+const cancelBtn = document.getElementById('Cancel')
+const saveBtn = document.getElementById('Save')
+
+cancelBtn.addEventListener('click', cancelBtnRemoveMarker)
+saveBtn.addEventListener('click', saveNewCoordToBackend)
+
+function cancelBtnRemoveMarker(){
+    if (mapMarker.length > 2){
+        let bufMarker = mapMarker[mapMarker.length - 1]
+        let markerToRemove = mapMarker[mapMarker.length - 2]
+        markerToRemove.remove()
+        mapMarker.pop()
+        mapMarker.pop()
+        mapMarker.push(bufMarker)
+        polylinePrinter(mapMarker)
+    }else{
+        console.log('nothink to cancel')
+    } 
+}
+
+function saveNewCoordToBackend(){
+    let arrayToSave = [...mapMarker]
+
+    if (mapMarker.length > 2){
+        arrayToSave.pop()
+        arrayToSave.shift()
+    }else{
+        arrayToSave = []
+    }
+    console.log('new Marker -', arrayToSave);
 }
