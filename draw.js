4f75220fba73a7ff197996cb7350d1b03b6fd546
aissilen <ivanlobanov97@gmail.com>
2021-02-03 00:49:40 +0800

    - ненужные штуки

--- draw.js
+++ draw.js
@@ -53,7 +53,7 @@ let ats_coord = ["52.27658", "104.248222"]
 
 let usi_marker = new L.Marker(usi_coord,{icon:offlineRsIcon,type: 'usi'}).addTo(feautures)
 let ats_marker = new L.Marker(ats_coord,{icon:onlineAtsIcon,type: 'ats'}).addTo(feautures)
-let polyline = L.polyline([ats_coord,usi_coord], {color: 'red'}).addTo(map)
+let polyline = L.polyline([ats_coord,usi_coord], {weight: 3,dashArray: '2,5',dashOffset:0.2}).addTo(feautures)
 
 feautures.addTo(map)
 map.fitBounds(polyline.getBounds())
@@ -61,60 +61,67 @@ map.fitBounds(polyline.getBounds())
 editableLayers.addLayer(polyline)
 map.addLayer(editableLayers)
 
-let sidebar = L.control.sidebar('sidebar', {
-    position: 'right'
-})
-
-let options = {
-    position: 'topleft',
-    draw: false,
-    edit: {
-        featureGroup: editableLayers, //REQUIRED!! 
-        remove: false
-    }
-};
-
-var drawControl = new L.Control.Draw(options)
-map.addControl(drawControl)
+let sidebar = L.control.sidebar('sidebar', {position: 'right'})
 map.addControl(sidebar)
 
-console.log(drawControl)
+const editor_handler = new L.EditToolbar.Edit(map, {featureGroup: editableLayers})
 
+const edit_features_button = document.getElementById('edit_features'),
+save_features_button = document.getElementById('save_button')
+
+edit_features_button.onclick = () => {
+    let classes = edit_features_button.classList
+    classes.toggle('active')
+    if (classes.contains("active")){
+        editor_handler.enable()
+    }else{
+        editor_handler.save()
+        editor_handler.disable()
+    }
+}
+
+save_features_button.onclick = () => {
+    let geoJson = feautures.toGeoJSON()
+    console.log(JSON.stringify(geoJson))
+}
 
 map.on('draw:edited', e => {
-    var layers = e.layers;
-    console.log(e)
-    layers.eachLayer(layer => {
-        //do whatever you want; most likely save back to db
-        let features_lat_lng = layer.getLatLngs()
-
-        features_lat_lng.forEach( (el,index,array) => {
-            let feature
-            if (index != array.length -1 ){
-                if (index != 0){
-                    let point = new L.Marker(array[index],{icon:pointIcon,type: 'point'}).addTo(feautures)
-                    feature = _create_feature_properties(point)
-                    feature.properties["type"] = "point"
+    var layers = e.layers
+    console.log('edit finished')
+    if (Object.keys(layers._layers) != 0 ){
+        feautures.clearLayers()
+        layers.eachLayer(layer => {
+            //do whatever you want; most likely save back to db
+            let features_lat_lng = layer.getLatLngs()
+    
+            features_lat_lng.forEach( (el,index,array) => {
+                let feature
+                if (index != array.length -1 ){
+                    if (index != 0){
+                        let point = new L.Marker(array[index],{icon:pointIcon,type: 'point'}).addTo(feautures)
+                        feature = _create_feature_properties(point)
+                        feature.properties["type"] = "point"
+                    }
+                    let polyline = L.polyline([array[index],array[index+1]], {color: colors.canalization,weight: 5}).addTo(feautures)
+                    _create_color_change_event(polyline)
+    
+                    feature = _create_feature_properties(polyline)
+                    feature.properties["type"] = "canalization"
+                    feature.properties["order"] = index+1
+                    feature.properties["length"] = ''
+                }else{
+                    usi_marker.setLatLng(el).addTo(feautures)
+                    feature = _create_feature_properties(usi_marker)
+                    feature.properties["type"] = "rs"
+                    ats_marker.setLatLng(array[0]).addTo(feautures)
+                    feature = _create_feature_properties(ats_marker)
+                    feature.properties["type"] = "ats"
                 }
-                let polyline = L.polyline([array[index],array[index+1]], {color: colors.canalization}).addTo(feautures)
-                _create_color_change_event(polyline)
-
-                feature = _create_feature_properties(polyline)
-                feature.properties["type"] = "canalization"
-                feature.properties["order"] = index+1
-                feature.properties["length"] = ''
-            }else{
-                usi_marker.setLatLng(el).addTo(feautures)
-                feature = _create_feature_properties(usi_marker)
-                feature.properties["type"] = "rs"
-                ats_marker.setLatLng(array[0]).addTo(feautures)
-                feature = _create_feature_properties(ats_marker)
-                feature.properties["type"] = "ats"
-            }
+            })
+            layer.remove()
         })
-        layer.remove()
-        feautures.addTo(map)
-    })
+    }
+    feautures.addTo(map)
 })
 
 function _create_feature_properties(element){
@@ -137,18 +144,13 @@ function _create_color_change_event(element){
 
 map.on('draw:editstart', e =>{
     console.log('_______________edit started______________')
-
     sidebar.hide()
-
     feautures.remove()
-    feautures.clearLayers()
     polyline.addTo(map)
 })
 
-radio_color.addEventListener('change',ev=>{
+radio_color.addEventListener('change',()=>{
     let value = radio_color.color.value
-
-    console.log(value, selected_feature);
     selected_feature.options.color = colors[value]
     selected_feature.redraw()
 })
@@ -157,11 +159,8 @@ function getKeyByValue(object, value) {
     return Object.keys(object).find(key => object[key] === value)
 }
 
-document.getElementById('save_route_button').addEventListener('click', ev => {
-    ev.preventDefault()
-    let geoJson = feautures.toGeoJSON()
-    console.log(JSON.stringify(geoJson))
-})
+
+
 
 // let jsonLayer = L.geoJson(geojsonFeature, {
 //     onEachFeature: function (feature, layer) {
